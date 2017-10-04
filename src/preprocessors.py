7c648369ab17b832d3ce850af15c9888c37746fc
Christopher Gundler <c.gundler@mail.de>
2017-10-05 08:00:02 +1300

    Add 'RemoveMultipleChars'

--- src/preprocessors.py
+++ src/preprocessors.py
@@ -1,6 +1,7 @@
 """ A module for handling dynamic chained preprocessors. """
 
 from abc import ABC, abstractmethod
+from itertools import groupby
 
 
 class Preprocessor(ABC):
@@ -32,4 +33,18 @@ class RemoveUrls(Preprocessor):
         for tweet in self.tweets:
             tweet.text = [x if not x.startswith(
                 'http') else 'URL' for x in tweet.text]
+            yield tweet
+
+
+class RemoveMultipleChars:
+    "Remove multiple, equal characters ('HIIII!!!!' -> 'HI!')"
+
+    def __init__(self, tweets):
+        """ Creates a new preprocessor. """
+        self.tweets = tweets
+
+    def __iter__(self):
+        for tweet in self.tweets:
+            tweet.text = [''.join(ch for ch, _ in groupby(word))
+                          for word in tweet.text]
             yield tweet
