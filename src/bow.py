34278a8dca3d53abad6c46dc81a43a06c46e0324
Christopher Gundler <c.gundler@mail.de>
2017-10-12 23:21:35 +1300

    Smoothly intergration into the pipeline

--- src/bow.py
+++ src/bow.py
@@ -1,24 +1,38 @@
 from collections import Counter
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
-class BagOfWords:
-    def __init__(self, data=[]):
-        self.bow = Counter()
-        for sample in data:
-            self.add(sample)
+class BagOfWords(BaseEstimator, TransformerMixin):
+    def __init__(self, min_occurrences=2, max_features=None):
+        self.counter = Counter()
+        self.min_occurrences = min_occurrences
+        self.max_features = max_features
+        self.bow = None
 
-    def add(self, data):
-        self.bow.update(data)
+    def fit(self, X, y=None):
+        for x in X:
+            self.counter.update(x)
 
-    def compute(self, min_occurrences=1, max_features=None):
-        output = {}
-        counter = 2
-        for word, occurences in self.bow.most_common(max_features):
-            if occurences >= min_occurrences:
-                output[word] = counter
-                counter += 1
+        self.bow = {}
+        i = 2
+        for word, occurences in self.counter.most_common(self.max_features):
+            if occurences >= self.min_occurrences:
+                self.bow[word] = i
+                i += 1
+
+    def transform(self, X, y=None):
+        if self.bow is None:
+            raise RuntimeError("Fitting required before transform!")
+
+        output = []
+        for x in X:
+            output.append(
+                [self.bow[word] if word in self.bow else 1 for word in x])
         return output
 
-    @staticmethod
-    def transform(tokenized_text, bag_of_words):
-        return [bag_of_words[word] if word in bag_of_words else 1 for word in tokenized_text]
+    def fit_transform(self, X, y=None):
+        self.fit(X)
+        return self.transform(X)
+
+    def size(self):
+        return len(self.bow)
